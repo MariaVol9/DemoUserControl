95b0875e91e910a730a85c592be21505a5af9af4
margolevi977@gmail.com <margolevi977@gmail.com>
2020-07-18 23:36:54 +0300

    Add project files.

---
+++ DemoUserControl/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoUserControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("State id = {0}, name = {1}", ucState1.SelectedState.ID, ucState1.SelectedState.Name), "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ DemoUserControl/ucState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoUserControl
+{
+    public partial class ucState : UserControl
+    {
+        public ucState()
+        {
+            InitializeComponent();
+        }
+        public States SelectedState
+        {
+            get
+            {
+                return (States)comboBox1.SelectedItem;
+            }
+        }
+
+        private void ucState_Load(object sender, EventArgs e)
+        {
+            List<States> list = new List<States>();
+            list.Add(new States() { ID = 1, Name = "Delhi" });
+            list.Add(new States() { ID = 2, Name = "Bihar" });
+            list.Add(new States() { ID = 3, Name = "Punjab" });
+            list.Add(new States() { ID = 4, Name = "UP" });
+            comboBox1.DataSource = list;
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "Name";
+        }
+    }
+}
